8b698a9be2cd6a4b7cf0645a0460e1e295dfb934
vtsuellen <vtsuellen@hotmail.com>
2024-02-06 16:51:14 -0300

    feat: transformar conteudo da lista em um dicionario

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-
+    for i in range(len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            print(f'Arquivo {path_file} já importado')
+            return
+    file = txt_importer(path_file)
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file),
+        'linhas_do_arquivo': file
+    }
+    instance.enqueue(data)
+    print(data)
 
 def remove(instance):
     """Aqui irá sua implementação"""
