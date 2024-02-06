b1c49cd2dce34d9873047c20085022973444375a
vtsuellen <vtsuellen@hotmail.com>
2024-02-06 11:37:58 -0300

    feat: função para importar noticias a partir de um arquivo txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    list = []
+    try:
+        with open(path_file, 'r') as file:
+            if not path_file.endswith('.txt'):
+                sys.stderr.write('Formato inválido')
+            for line in file:
+                list.append(line.strip())
+            return list
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
